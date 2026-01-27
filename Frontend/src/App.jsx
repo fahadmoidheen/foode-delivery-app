0e63f4ac476c5e16954a68509dbb01c0d94c83c8
Karthika R <karthikar@Karthikas-MacBook-Air.local>
2026-01-27 13:09:24 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App 
---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="header-contents">
+                    <h2>Order Your favuorite food here</h2>
+                    <button>View mneu</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
